80a6f83a92ade6f7d09a2b866fe18124a9036d76
Seyyu0015 <seyyu0015@163.com>
2022-05-07 10:25:22 +0800

    完善网页格式 外链css 完成更改页面内容的方法

--- display.py
+++ display.py
@@ -1,12 +1,25 @@
-from pyecharts import *
-from pyecharts.charts import Bar
+#!/usr/bin/python3
+from bs4 import BeautifulSoup
+
+
+# 刷新显示页面的方法 将第num个图片替换为 img_src；将第num个文字替换为Con
+def display_change(num: int, img_src: str, con: int):
+    with open("display_html.html", "r") as f:
+        contents = f.read()
+        soup = BeautifulSoup(contents, 'lxml')
+
+        img = soup.find('img', id='img' + str(num))
+        img['src'] = 'userface/' + img_src
+
+        text = soup.find('td', id='text' + str(num))
+        text.string = str(con)
+
+        meta = soup.find('meta')
+        meta['charset'] = 'gb18030'
+
+    with open("display_html.html", "w") as fh:
+        fh.write(soup.prettify())
+
 
 if __name__ == '__main__':
-
-    bar = Bar("成绩柱形图", "副标题")
-    # 用于添加图表的数据和设置各种配置项
-    # is_more_utils=True可以按右边的下载按钮将图片下载到本地
-
-    bar.add("成绩表", ["语文", "数学", "英语", "物理", "化学", "生物"], [88, 90, 92, 87, 83, 81], is_more_utils=True)
-    bar.show_config()  # 打印输出图表的所有配置项
-    bar.render('E:\\pye\\bar.html')  # 在指定目录下生成一个 bar.html 的文件
+    display_change(1, '冰糖糯米.png', 2)
