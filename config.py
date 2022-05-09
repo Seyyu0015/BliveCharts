b6f388a49597ccd117bb6bf359424f6c0e02351c
SeyyuThePineApple <follow0015@gmail.com>
2022-05-09 11:19:05 +0800

    新增头像下载失败后补救机制，减少头像无法显示的可能性 fix 辣条不能正确增加贡献值

--- config.py
+++ config.py
@@ -4,14 +4,16 @@
 +clear_photos_pertime： Ture每次启动清空头像（可刷新头像） False不清空头像
 """
 roomid = 8763308
-clear_photos_pertime = False
+clear_photos_pertime = True
 
 """
 
 +rank_add_by_danmu：每发送一个弹幕所增加的贡献值。
++price: 礼物价值和增加贡献的比值（金瓜子：贡献）
 +freegift： 免费礼物增加的贡献值,0为不增加。
 +number：排行榜显示个数 0:不限制显示个数 建议不要修改。
 """
 rank_add_by_danmu = 1
+price = 100
 freegift = 1
 number = 9
--- main.py
+++ main.py
@@ -14,6 +14,9 @@ import rank
 # 创建直播间对象
 room = live.LiveDanmaku(config.roomid)
 
+# 用于检查头像的字典
+user_dict = {}
+
 # 清空储存用户头像的文件夹以刷新用户头像
 if config.clear_photos_pertime:
     shutil.rmtree('./userface')
@@ -26,15 +29,19 @@ display.reset()
 # 弹幕触发方法
 @room.on('DANMU_MSG')
 async def on_danmaku(event):
+    global user_dict
     # 获取发送弹幕的用户的信息
     user_id = int(event['data']['info'][2][0])  # 用户id
     user_display_name = event['data']['info'][2][1]  # 用户昵称
 
     try:
         # 调用增加贡献的方法
-        rank.add_user_dict(event['data']['info'][2][1], config.rank_add_by_danmu)
+        rank.add_user_dict(user_display_name, config.rank_add_by_danmu)
+
         # 调用爬取头像的方法
-        await photo.get_user_face(user_id, user_display_name)
+        user_dict[user_id] = user_display_name
+        for key, value in user_dict.items():
+            await photo.get_user_face(key, value)
 
     except:
         pass
@@ -47,10 +54,10 @@ async def on_gift(event):
     try:
         # 修复辣条意外价值为100的问题
         if event['data']['data']['giftName'] == '辣条':
-            rank.add_user_dict(event['data']['data']['uname'], 0)
+            rank.add_user_dict(event['data']['data']['uname'], config.freegift)
         # 调用增加贡献的方法
         else:
-            rank.add_user_dict(event['data']['data']['uname'], event['data']['data']['price']/100)
+            rank.add_user_dict(event['data']['data']['uname'], event['data']['data']['price'] / config.price)
         # 调用爬取头像的方法
         await photo.get_user_face(event['data']['data']['mid'])
     except:
